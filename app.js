746f7cebaa7bd2c34b4bc9480afc0acbad8797d2
yiwen950725 <tiffany-gu@hotmail.com>
2021-04-03 20:28:31 -0700

    new functions

--- app.js
+++ app.js
@@ -31,12 +31,7 @@ app.use(express.urlencoded({
     extended: true
 }))
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    next();
-});
+app.use(cors());
 
 // ===================
 // MONGO DB
