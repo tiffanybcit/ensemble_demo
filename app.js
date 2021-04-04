9b217e7760757eae95c16d9fa2e9e1a52bb69119
yiwen950725 <tiffany-gu@hotmail.com>
2021-04-03 20:39:38 -0700

    new functions

--- app.js
+++ app.js
@@ -51,7 +51,25 @@ const {
 // ===================
 var cors = require('cors');
 app.use(cors());
+// Add headers
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://ensemble-tiffany-demo.herokuapp.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // ===================
 // EXPRESS TRIGGER
