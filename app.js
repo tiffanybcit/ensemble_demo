e54645abf81a9df30bcb1838d9c1c3c43ba3ab6d
yiwen950725 <tiffany-gu@hotmail.com>
2021-04-03 19:54:07 -0700

    new functions

--- app.js
+++ app.js
@@ -61,6 +61,11 @@ const {
 var cors = require('cors');
 app.use(cors());
 app.options('*', cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // ===================
 // EXPRESS TRIGGER
