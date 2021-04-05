b1084645ac908b41163440669a5205bb6c6d1668
yiwen950725 <tiffany-gu@hotmail.com>
2021-04-05 01:53:47 -0700

    deleted some console.logs

--- app.js
+++ app.js
@@ -256,6 +256,4 @@ app.delete("/deleteTask", (req, res) => {
 });
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
+app.listen(port);
